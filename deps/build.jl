286356351100b9c39106e85ae9dceb09beb11b86
Jason Knight <Jason@JasonKnight.us>
2014-04-03 17:54:16 -0500

    Tidying

--- deps/build.jl
+++ deps/build.jl
@@ -5,5 +5,6 @@ using BinDeps
 libdai = library_dependency("libdai")
 
 #provides(Sources,URI("http://staff.science.uva.nl/~jmooij1/libDAI/libDAI-0.3.1.tar.gz"), libdai)
+## will need to modify the libdai config file to enable JunctionTree algorithm
 
 @BinDeps.install
